6e57fbb5bf797e37aa5bc4c1915ca38bba9f39d6
jacob grigorev <grigorevakov30@gmail.com>
2024-04-25 17:40:01 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['яблоко', 'мандарин', 'арбуз', 'киви', 'дыня', 'вишня', 'персик']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list.pop(2)
+my_list.insert(2, 'груша')
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict.update([('orange', 'оранжевый')])
+print(my_dict)
+
