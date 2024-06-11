51faedac73bdf85b9eedb5ddaf055947bbb3668d
jacob grigorev <grigorevakov30@gmail.com>
2024-06-11 13:00:37 +0300

    file_opening_modes.py

--- file_opening_modes.py
+++ file_opening_modes.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 file_name = "homework.txt"
-file = open(file_name, mode='rb')
+file = open(file_name, mode='r', encoding='utf-8')
 file_content = file.read()
 file.close()
 pprint(file_content)
