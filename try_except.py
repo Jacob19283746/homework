f169b40debca1cbb5bd617eaaaff07474935bfae
jacob grigorev <grigorevakov30@gmail.com>
2024-06-19 18:10:43 +0300

    try_except.py

--- try_except.py
+++ try_except.py
@@ -1,10 +1,11 @@
-def add_everything_up(a, b):
+def add_everything_up(a, b):              # функция сложения разных типов данных
     try:
         return a + b
     except TypeError:
         return str(a) + str(b)
 
 
-print(add_everything_up(123.456, 'строка'))
+print(add_everything_up(123.456, 'строка'))  #обрабатывается except
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+print(add_everything_up(3, 3))              # обрабатывается try
